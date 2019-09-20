5e83a97558ff6952de3476894fd647032f636976
Steven Rzenca <ocean.stevy@gmail.com>
2019-09-20 19:38:06 +0200

    Added CC, GameManager, UiManager, Ui

---
+++ SAEDiplomaProject/Assets/_Scripts/_GameValues/UiManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    #region MemberVariables
+    private GameObject m_Crosshair;
+    private UiManager m_Instance;
+    #endregion MemberVariables
+    #region Properties
+    /// <summary>
+    /// this is the Corsshair we're aiming with
+    /// </summary>
+    public GameObject Crosshair { get => m_Crosshair; set => m_Crosshair = value; }
+    /// <summary>
+    /// This Values is need for our singleton, so we have acces from everywhere
+    /// </summary>
+    public UiManager Instance { get => m_Instance; set => m_Instance = value; }
+    #endregion Properties
+
+    private void Awake()
+    {
+        //Creates Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Crosshair = GameObject.Find("UiCorsshair");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
