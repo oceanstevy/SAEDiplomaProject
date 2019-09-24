4dccd6147d32466dfbf9515ba1a71a4d6f913ccd
Will Dores <cornpopper@hotmail.com>
2019-09-25 00:23:55 +0200

    fixed my script, ui looking sick

--- SAEDiplomaProject/Assets/Ui Scenes_Assets/Ui Assets/Ui Code/Menu_Controller.cs
+++ SAEDiplomaProject/Assets/Ui Scenes_Assets/Ui Assets/Ui Code/Menu_Controller.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Menu_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject _Menu_Panel;
+    public GameObject _Help_Panel;
+    public GameObject _Options_Panel;
+    public GameObject _Credits_Panel;
+
+    private void Start()
     {
-        
+        _Menu_Panel = GameObject.Find("MenuPanel");
+        _Help_Panel = GameObject.Find("HelpPanel");
+        _Options_Panel = GameObject.Find("OptionsPanel");
+        _Credits_Panel = GameObject.Find("CreditsPanel");
+
+        _Menu_Panel.SetActive(true);
+        _Help_Panel.SetActive(false);
+        _Options_Panel.SetActive(false);
+        _Credits_Panel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Open_Menu()
     {
-        
+        _Menu_Panel.SetActive(true);
+        _Help_Panel.SetActive(false);
+        _Options_Panel.SetActive(false);
+        _Credits_Panel.SetActive(false);
+    }
+
+    public void Open_Help()
+    {
+        _Menu_Panel.SetActive(false);
+        _Help_Panel.SetActive(true);
+        _Options_Panel.SetActive(false);
+        _Credits_Panel.SetActive(false);
+    }
+
+    public void Open_Options()
+    {
+        _Menu_Panel.SetActive(false);
+        _Help_Panel.SetActive(false);
+        _Options_Panel.SetActive(true);
+        _Credits_Panel.SetActive(false);
+    }
+
+    public void Open_Credits()
+    {
+        _Menu_Panel.SetActive(false);
+        _Help_Panel.SetActive(false);
+        _Options_Panel.SetActive(false);
+        _Credits_Panel.SetActive(true);
     }
 }
