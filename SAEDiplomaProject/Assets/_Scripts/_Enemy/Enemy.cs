7d8743c51be6a20dc13945da1d8d66fe4808d9ed
Jannek Vieth <jannekvieth@outlook.de>
2019-09-24 17:01:41 +0200

    Enemy has Idle Movement, and Path now
    If Player is not seen, Enemy goes on with his Idle Path

--- SAEDiplomaProject/Assets/_Scripts/_Enemy/Enemy.cs
+++ SAEDiplomaProject/Assets/_Scripts/_Enemy/Enemy.cs
@@ -30,7 +30,7 @@ public class Enemy : MonoBehaviour
     /// <summary>
     /// Enemy Movementspeed
     /// </summary>
-    private int m_Movementspeed = 10;
+    private int m_Movementspeed = 2;
     /// <summary>
     /// Idle Waypoints for Enemy
     /// </summary>
@@ -40,6 +40,8 @@ public class Enemy : MonoBehaviour
     private int CurrentWaypoint;
 
     private FieldOfView FoV;
+
+    private bool GoingForward = false;
     #endregion PrivateVariables
 
     // Start is called before the first frame update
@@ -69,28 +71,24 @@ public class Enemy : MonoBehaviour
             Dir = Dir.normalized;
             Dir = Dir * Time.deltaTime * m_Movementspeed;
 
-            // Im Standard nutzen wir den lokalen Space bei Transform.Translate
             transform.Translate(Dir, Space.World);
             transform.forward = Vector3.RotateTowards(transform.forward, Dir.normalized, Mathf.PI * Time.deltaTime, 0);
         }
         else
         {
-            /*Vector3 dir = m_Waypoints[CurrentWaypoint].position - transform.position;
+            Vector3 Direction = m_Waypoints[CurrentWaypoint].position - transform.position;
 
-            // Falls die Schrittlänge größer ist als die Entfernung zum Ziel
-            // normalisierte Vektoren haben die Länge 1, behalten aber ihre Richtung
-            dir = dir.normalized;
-            dir = dir * Time.deltaTime * m_Movementspeed;
-            if (dir.magnitude > Vector3.Distance(m_Waypoints[CurrentWaypoint].position, transform.position))
+            Direction = Direction.normalized;
+            Direction = Direction * Time.deltaTime * m_Movementspeed;
+            if (Direction.magnitude > Vector3.Distance(m_Waypoints[CurrentWaypoint].position, transform.position))
             {
-                //m_GoingForward = !m_GoingForward;
-                // Modulo hilft hier um kein out of range zu bekommen
-                CurrentWaypoint = (CurrentWaypoint + m_Waypoints.Length) % m_Waypoints.Length;
+                CurrentWaypoint = (CurrentWaypoint +
+                                        (GoingForward ? 1 : -1) +
+                                        m_Waypoints.Length)
+                                % m_Waypoints.Length;
             }
-
-            // Im Standard nutzen wir den lokalen Space bei Transform.Translate
-            transform.Translate(dir, Space.World);
-            transform.forward = Vector3.RotateTowards(transform.forward, dir.normalized, Mathf.PI * Time.deltaTime, 0);*/
+            transform.Translate(Direction, Space.World);
+            transform.forward = Vector3.RotateTowards(transform.forward, Direction.normalized, Mathf.PI * Time.deltaTime, 0);
         }
     }
 }
