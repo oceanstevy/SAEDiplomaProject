b72b43318f1792389ff8c77e2309d16f9e02b873
Jannek Vieth <jannekvieth@outlook.de>
2019-09-25 15:40:00 +0200

    Started PickUpItem Class, needs to be finished

--- SAEDiplomaProject/Assets/_Scripts/_CC/_Inventory/PickupItem.cs
+++ SAEDiplomaProject/Assets/_Scripts/_CC/_Inventory/PickupItem.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PickupItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region MemberVirables
+    private Enums.Item m_Item;
+    private Enums.ItemType m_Type;
+    private short m_Quantity;
+    private Material m_Material;
+    #endregion MemberVirables
+    #region Properties
+    /// <summary>
+    /// What Kind of Item
+    /// </summary>
+    public Enums.Item Item { get => m_Item; set => m_Item = value; }
+    /// <summary>
+    /// What Item Type
+    /// </summary>
+    public Enums.ItemType Type { get => m_Type; set => m_Type = value; }
+    /// <summary>
+    /// The Amount from said Item
+    /// </summary>
+    public short Quantity { get => m_Quantity; set => m_Quantity = value; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public Material Material { get => m_Material; set => m_Material = value; }
+    #endregion Properties
+
+    private Renderer ObjectRenderer;
+    private Material OriginalMaterial;
+
+    private void Awake()
     {
-        
+        ObjectRenderer = GetComponent<Renderer>();
+        OriginalMaterial = GetComponent<Material>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        ObjectRenderer.material = OriginalMaterial;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ObjectRenderer.material = m_Material;
     }
 }
