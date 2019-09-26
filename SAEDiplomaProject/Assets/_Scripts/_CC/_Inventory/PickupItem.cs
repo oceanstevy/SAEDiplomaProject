e6e5ed5971d1c302d40d58614b2d96c168d35f05
Jannek Vieth <jannekvieth@outlook.de>
2019-09-26 12:54:49 +0200

    Added Item and EnemyPrefab. Added Spotlight for Item.

--- SAEDiplomaProject/Assets/_Scripts/_CC/_Inventory/PickupItem.cs
+++ SAEDiplomaProject/Assets/_Scripts/_CC/_Inventory/PickupItem.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class PickupItem : MonoBehaviour
 {
     #region MemberVirables
-    private Enums.Item m_Item;
+    [Header("Item Type")]
+    [SerializeField]
     private Enums.ItemType m_Type;
+    [SerializeField]
+    private Enums.Item m_Item;
+    [SerializeField]
     private short m_Quantity;
     private Material m_Material;
     #endregion MemberVirables
     #region Properties
     /// <summary>
-    /// What Kind of Item
-    /// </summary>
-    public Enums.Item Item { get => m_Item; set => m_Item = value; }
-    /// <summary>
     /// What Item Type
     /// </summary>
     public Enums.ItemType Type { get => m_Type; set => m_Type = value; }
+    /// <summary>
+    /// What Kind of Item
+    /// </summary>
+    public Enums.Item Item { get => m_Item; set => m_Item = value; }
     /// <summary>
     /// The Amount from said Item
     /// </summary>
@@ -29,22 +33,27 @@ public class PickupItem : MonoBehaviour
     public Material Material { get => m_Material; set => m_Material = value; }
     #endregion Properties
 
-    private Renderer ObjectRenderer;
-    private Material OriginalMaterial;
+    private Light SpotLight;
 
     private void Awake()
     {
-        ObjectRenderer = GetComponent<Renderer>();
-        OriginalMaterial = GetComponent<Material>();
+        SpotLight = GetComponentInChildren<Light>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        ObjectRenderer.material = OriginalMaterial;
+        if (other.tag != "Player")
+            return;
+
+        SpotLight.gameObject.SetActive(false);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        ObjectRenderer.material = m_Material;
+        if (other.tag != "Player")
+            return;
+
+        SpotLight.gameObject.SetActive(true);
+
     }
 }
