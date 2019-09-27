d1a941cb461ffb5505de1b15f4e6fe892bbc3f5c
Jannek Vieth <jannekvieth@outlook.de>
2019-09-27 13:58:48 +0200

    Added NavMesh Component for Enemy, Added Scripts for State Machine

---
+++ SAEDiplomaProject/Assets/_Scripts/_Enemy/_Pathing/NavGrid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavGrid : MonoBehaviour
+{
+    public static NavGrid Get { get; private set; }
+    public LayerMask GroundLayer;
+    NavNode[,] AllNodes;
+
+    private void Awake()
+    {
+        if(Get == null)
+        {
+            Get = this;
+        }
+        else
+        {
+            return;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void CreateGrid()
+    {
+        RaycastHit Hit;
+
+        AllNodes = new NavNode[101, 101];
+    }
+}
---
+++ SAEDiplomaProject/Assets/_Scripts/_Enemy/_Pathing/NavNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavNode
+{
+    public readonly List<NavNode> Neighbours = new List<NavNode>();
+
+    #region PrivateVariables
+    private Vector3 Position;
+    private bool Walkable;
+    private float GroundCost;
+    private float HeuristicCost;
+    private NavNode Previous;
+    #endregion PrivateVariables
+    #region Properties
+    public float CompleteCost { get { return GroundCost + HeuristicCost; } }
+    #endregion Properties
+
+    public NavNode(Vector3 Pos, bool Walk)
+    {
+        Position = Pos;
+        Walkable = Walk;
+    }
+}
